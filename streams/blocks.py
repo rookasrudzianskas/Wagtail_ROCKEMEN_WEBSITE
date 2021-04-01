f2c0affe95fbba6b1cfb8d9c78381b451b6fd237
Rokas <rookas.rudzenskas@gmail.com>
2021-04-01 16:48:38 +0300

    Rokas

--- streams/blocks.py
+++ streams/blocks.py
@@ -15,10 +15,21 @@ class TitleBlock(blocks.StructBlock):
         help_text = "Centered text to display on the page"
 
 
+class LinkValue(blocks.StructValue):
+    def url(self) -> str:
+        internal_page = self.get("internal_page")
+        external_link = self.get("external_page")
+        if internal_page:
+            return internal_page.url
+        elif external_link:
+            return external_link
+        return ""
+
+
 class Link(blocks.StructBlock):
     link_text = blocks.CharBlock(
         max_length=50,
-        default = 'More details'
+        default='More details'
     )
 
     internal_page = blocks.PageChooserBlock(
@@ -28,6 +39,9 @@ class Link(blocks.StructBlock):
     external_link = blocks.URLBlock(
         required=False
     )
+
+    class Meta:
+        value_class = LinkValue
 
 
 class Card(blocks.StructBlock):
@@ -45,9 +59,6 @@ class Card(blocks.StructBlock):
     )
 
     link = Link(help_text="Enter the link or selected a page")
-
-
-
 
 
 class CardsBlock(blocks.StructBlock):
