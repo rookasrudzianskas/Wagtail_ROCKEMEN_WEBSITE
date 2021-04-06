b90c5c4b3c5f71cbaf8bcce94d27ea645f26b789
Rokas <rookas.rudzenskas@gmail.com>
2021-04-06 18:17:33 +0300

    Rokas

--- flex/models.py
+++ flex/models.py
@@ -1,7 +1,36 @@
+from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
+from wagtail.snippets.blocks import SnippetChooserBlock
+
+from home.models import new_table_options
+from streams import blocks
+from wagtail.core import blocks as wagtail_blocks
 
 
 class FlexPage(Page):
+    body = StreamField([
+        ("title", blocks.TitleBlock()),
+        ("cards", blocks.CardsBlock()),
+        ("image_and_text", blocks.ImageAndTextBlock()),
+        ("cta", blocks.CallToActionBlock()),
+        ("testimonial", SnippetChooserBlock(
+            target_model='testimonials.Testimonial',
+            template="streams/testimonial_block.html",
+        )),
+        ('pricing_table', blocks.PricingTableBlock(
+            table_options=new_table_options,
+        )),
+        ('richtext', wagtail_blocks.RichTextBlock(
+           template="streams/simple_richtext_block.html",
+        )),
+
+    ], null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body'),
+    ]
+
     class Meta:
         verbose_name = "Flex (misc) Page"
         verbose_name_plural = "Flex (misc) Pages"
