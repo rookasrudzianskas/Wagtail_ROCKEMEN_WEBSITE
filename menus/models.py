161259c108d4c7cea3b9a7050e3ef71ba1733379
Rokas <rookas.rudzenskas@gmail.com>
2021-04-09 15:57:50 +0300

    Rokas

--- menus/models.py
+++ menus/models.py
@@ -28,6 +28,21 @@ class MenuItem(Orderable):
 
     page = ParentalKey("Menu", related_name="menu_items")
 
+    @property
+    def link(self) -> str:
+        if self.link_page:
+            return self.link_page.url
+        elif self.link_url:
+            return self.link_url
+        return "#"
+
+    @property
+    def title(self):
+        if self.link_page and not self.link_title:
+            return self.link_page.title
+        elif self.link_title:
+            return self.link_title
+        return "Missing title"
 
 class Menu(ClusterableModel):
     title = models.CharField(max_length=100)
