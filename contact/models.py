a466f04378ed9977d222de117f27b4c0156df2a7
Rokas <rookas.rudzenskas@gmail.com>
2021-04-12 16:51:30 +0300

    Rokas

--- contact/models.py
+++ contact/models.py
@@ -1,9 +1,9 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
-from modelcluster.fields import ParentalKey
+from modelcluster.models import ParentalKey
 from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
-from wagtail.contrib.forms.models import AbstractEmailForm
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
 from wagtail.core.fields import RichTextField
 from wagtail.images.edit_handlers import ImageChooserPanel
 
@@ -23,11 +23,22 @@ class ContactPage(AbstractEmailForm):
     max_count = 1
 
     intro = RichTextField(blank=True, features=["bold", "link", "ol", "ul"])
-    thank_you_text = RichTextField(blank=True, features=["bold", "link", "ol", "ul"])
-    map_image = models.ForeignKey('wagtailimages.Image', null=True, blank=False, on_delete=models.SET_NULL,
-                                  help_text="Image will be cropped to 580px by 355px", related_name="+")
-    map_url = models.URLField(blank=True,
-                              help_text="Optional. If you provide link there  the map image will become a link")
+    thank_you_text = RichTextField(
+        blank=True,
+        features=["bold", "link", "ol", "ul"]
+    )
+    map_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        help_text='Image will be cropped to 580px by 355px',
+        related_name='+',
+    )
+    map_url = models.URLField(
+        blank=True,
+        help_text='Optional. If you provide a link here the map image will become a link'
+    )
 
     content_panels = AbstractEmailForm.content_panels + [
         FieldPanel("intro"),
